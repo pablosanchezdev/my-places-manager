0f5f86db3ea2990028fdd50df84fee713d35a00e
Pablo Sanchez Egido <pablosze@gmail.com>
2017-11-23 17:23:01 +0100

    Changed my lists page design

--- src/pages/my-lists/my-lists.ts
+++ src/pages/my-lists/my-lists.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { ItemSliding } from 'ionic-angular/components/item/item-sliding';
 
 @IonicPage()
 @Component({
@@ -8,10 +9,21 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class MyListsPage {
 
+  lists: object[];
+
   constructor(public navCtrl: NavController, public navParams: NavParams) { }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad MyListsPage');
+    this.lists = [
+      { name: 'Lista 1', description: 'Descripcion lista 1', numItems: 5 },
+      { name: 'Lista 2', description: 'Descripcion lista 2', numItems: 1 },
+      { name: 'Lista 3', description: 'Descripcion lista 3', numItems: 8 },
+      { name: 'Lista 4', description: 'Descripcion lista 4', numItems: 13 }
+    ];
   }
 
+  delete(slidingItem: ItemSliding, list) {
+    slidingItem.close();
+    this.lists.splice(this.lists.indexOf(list), 1);
+  }
 }
